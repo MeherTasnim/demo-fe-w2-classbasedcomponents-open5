33cbd3a8c70c42b25e653484802bc0fef95d8619
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2023-07-05 11:14:42 -0500

    Display quote data from API

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import React from 'react';
 class App extends React.Component {
     constructor() {
         super();
+        this.state = { quote: "" }; // initialize state
+    }
+
+    componentDidMount() {
+        fetch("https://api.kanye.rest").then((res) => res.json())
+            .then((data) => {
+                console.log(data); // debugging
+                this.setState({ quote: data.quote })
+            });
     }
 
     render() {
         return(
             <div>
-                Hello! I'm in a class-based component now.
+                <h1>Hello! I'm in a class-based component now.</h1>
+                <p>{ this.state.quote }</p>
             </div>
         )
     }
